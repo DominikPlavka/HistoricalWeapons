019fa61ab99511422fef9bc2561f8e3e42cbf134
Dominik Plavka <domino.plavka@gmail.com>
2023-05-18 14:21:42 +0200

    Initial setup of CI

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.scss"
+
+const Home = () => {
+
+    return (
+        <div className="main-container">
+            <h1 className="h1">Test heading</h1>
+            <p className="text">lhasdal layh sd</p>
+        </div>
+    )
+};
+
+export default Home;
