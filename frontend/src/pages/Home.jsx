eb8a21f367c46c206de448b07f117e4c56dcc052
Dominik Plavka <domino.plavka@gmail.com>
2023-05-24 12:36:13 +0200

    Minor CSS changes to headings

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
 
     return (
         <div className="main-container">
-            <h1>Test heading 1</h1>
-            <h2>Test heading 2</h2>
-            <h3>Test heading 3</h3>
-            <p>Paragraph text</p>
+            <h1>Historické zbrane</h1>
+            <h2>Chladné zbrane | </h2>
+            <h3>Podnadpis</h3>
+            <p>Text</p>
         </div>
     )
 };
