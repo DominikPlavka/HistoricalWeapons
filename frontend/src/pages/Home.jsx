2088a6807f97f1d2e6bff42f7fdfea37e1aba63f
Dominik Plavka <domino.plavka@gmail.com>
2023-05-24 13:10:15 +0200

    Changed content of home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import "./Home.scss"
 const Home = () => {
 
     return (
-        <div className="main-container">
+        <div className="main-container bright">
             <h1>Historické zbrane</h1>
-            <h2>Chladné zbrane | </h2>
+            <h2>Chladné zbrane</h2>
             <h3>Podnadpis</h3>
             <p>Text</p>
         </div>
