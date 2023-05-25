2b5b4291721f6dfced40ee5b2f9c9d7a25fa05e4
Dominik Plavka <domino.plavka@gmail.com>
2023-05-25 12:24:31 +0200

    Basic layout for headr done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,48 @@
-import "./Home.scss"
+import "./Home.scss";
+import { ArrowRightIcon, FaceIcon, ImageIcon, SizeIcon, SunIcon } from '@radix-ui/react-icons';
 
 const Home = () => {
 
     return (
         <div className="main-container bright">
-            <div className="image-container">
-                <h1>Historické zbrane</h1>
-                <hr />
-                <h2>Blablabla</h2>
-                <div className="menu-wrapper">
-                    <div className="menu">
-                        <h2>Chladné zbrane</h2>
-                        <h2>Palné zbrane</h2>
+            <div className="background-container">
+                <div className="header-container">
+                    <div>
+                        <h1>Historické zbrane</h1>
+                        <hr />
+                        <h2>Blablabla</h2>
+                    </div>
+                    <div className="menu-wrapper">
+                        <div className="menu">
+                            <div>
+                                <h2>Stopy histórie</h2>
+                                <p>Zbrane z rôznych období a </p>
+                            </div>
+                            <div className="icon-wrapper">
+                                <ArrowRightIcon
+                                    className="icon"
+                                />
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-            <div>
-                <p>sdfsdf</p>
+            <div><ArrowRightIcon /></div>
+            <div className="content-container">
+                <div className="header-container">
+                    <div>
+                        <h1>Historické zbrane</h1>
+                        <hr />
+                        <h2>Blablabla</h2>
+                    </div>
+                    <div className="menu-wrapper">
+                        <div className="menu">
+                            <h2>Chladné zbrane</h2>
+                            <h2>Palné zbrane</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
